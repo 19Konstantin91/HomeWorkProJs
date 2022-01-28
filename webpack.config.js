48b299f25e187c4fd84ffe004bc301f3d852163d
19Konstantin91 <kostya_klpov@mail.ru>
2022-01-28 19:42:02 +0300

    modified:   package-lock.json         modified:   package.json         modified:   public/template.html         modified:   src/index.js         new file:   src/style/main.scss         modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
@@ -11,11 +13,31 @@ module.exports = {
       filename: '[name].bundle.js',
     },
 
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+
     plugins: [
       new HtmlWebpackPlugin({
           title: 'GeekBrains Shop JS2',
           template: path.resolve(__dirname, './public/template.html'), // шаблон
           filename: 'index.html', // название выходного файла
       }),
+      new CleanWebpackPlugin(),
+      
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
     ],
+
 }
